09390e4c2c5c29a2fcf2a056ef476cf3eac1ebac
Samuel Ferla Iamarino <samuelf.iamarino@gmail.com>
2024-09-24 10:18:45 -0300

    Adicão Projetos Capitulo 2

---
+++ Projetos Praticos/EquacaoSegundoGrau/src/java/equacaoSegundoGrauServlets/ProcessarEquacaoServlet.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package equacaoSegundoGrauServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Samuel Iamarino
+ */
+@WebServlet(name = "ProcessarEquacaoServlet", urlPatterns = {"/processarEquacao"})
+public class ProcessarEquacaoServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Setting Enconding for UTF-8
+        request.setCharacterEncoding("UTF-8");
+        
+        //Variables
+        double a = Double.parseDouble(request.getParameter("a"));
+        double b = Double.parseDouble(request.getParameter("b"));
+        double c = Double.parseDouble(request.getParameter("c"));
+        double delta = (b * b) + (-4 * (a * c));
+        
+        if(delta < 0){
+            System.out.println("A equacao nao possui Raizes");
+        }else{
+            double x1 = ((-(b) + Math.sqrt(delta)) / 2 * a);
+            double x2 = ((-(b) - Math.sqrt(delta)) / 2 * a);
+            
+            System.out.println("As raizes sao: x1 = " + x1 + " x2 = " + x2);
+        }
+        
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
